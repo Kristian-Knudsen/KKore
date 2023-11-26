db94ab0b6a4da01dc57b8847082952f75c1d035c
Kristian-Knudsen <kristianpknudsen@gmail.com>
2023-11-27 00:04:08 +0100

    fixed export

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import KButton from './Components/KButton.vue';
 import KDropdown from './Components/KDropdown.vue';
 import KInput from './Components/KInput.vue';
 import KSwitch from './Components/KSwitch.vue';
-import KButtonGroupVue from './Components/KButtonGroup.vue';
-import KButtonStatedVue from './Components/KButtonStated.vue';
+import KButtonGroup from './Components/KButtonGroup.vue';
+import KButtonStated from './Components/KButtonStated.vue';
 
 export {
     KButton,
     KDropdown,
     KInput,
     KSwitch,
-    KButtonGroupVue,
-    KButtonStatedVue,
+    KButtonGroup,
+    KButtonStated,
 }
